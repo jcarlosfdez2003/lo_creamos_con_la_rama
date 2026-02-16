221ecc0c078240bb59300db6d1ad61542f28d090
Juan Carlos Fernandez <jcfg09042003@gmail.com>
2026-02-16 09:43:36 +0100

    Lo documentamos con javadoc

--- src/main/java/lo_creamos_con_la_rama/Logger.java
+++ src/main/java/lo_creamos_con_la_rama/Logger.java
@@ -1,5 +1,30 @@
 package lo_creamos_con_la_rama;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Logger {
 
+    private static final Logger INSTANCE = new Logger();
+    private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    private Logger() {
+
+    }
+
+    public static Logger getInstance() {
+        return INSTANCE;
+    }
+
+    public void info(String msg) {
+        System.out.println("[" + LocalDateTime.now().format(fmt) + "] [INFO] " + msg);
+    }
+
+    public void warn(String msg) {
+        System.out.println("[" + LocalDateTime.now().format(fmt) + "] [WARN] " + msg);
+    }
+
+    public void error(String msg) {
+        System.err.println("[" + LocalDateTime.now().format(fmt) + "] [ERROR] " + msg);
+    }
 }
